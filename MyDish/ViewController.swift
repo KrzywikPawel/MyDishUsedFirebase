bf6c5081f60153a7b41710ceaa8f87a5a72e3eb1
Pawel <pawel3007@vp.pl>
2019-11-30 16:24:54 +0100

    Initial Commit

---
+++ MyDish/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyDish
+//
+//  Created by Pawel Krzywik on 30/11/2019.
+//  Copyright © 2019 Pawel Krzywik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
