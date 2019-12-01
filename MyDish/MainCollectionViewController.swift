cc11aa0b8db7079f488fa8d87308de671d5c1da1
Pawel <pawel3007@vp.pl>
2019-12-01 18:18:23 +0100

    main clllection time has upper min, next i do intresting lvl show

--- MyDish/MainCollectionViewController.swift
+++ MyDish/MainCollectionViewController.swift
@@ -1,18 +1,27 @@
-//import UIKit
-//extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return arrayDish.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MainDishCollectionViewCell
-//        cell.dishImage.image = UIImage(named: "\(arrayDish[indexPath.row].image)")
-//        cell.nameLabel.text = arrayDish[indexPath.row].name
-//        cell.timeLabel.text = arrayDish[indexPath.row].time
-//        cell.levelLabel.text = arrayDish[indexPath.row].level
-//        return cell
-//    }
-//
-//
+import UIKit
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
     
-//}
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("se")
+        print(arrayDish.count)
+        return arrayDish.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MainDishCollectionViewCell
+        cell.dishImage.image = UIImage(named: "\(arrayDish[indexPath.row].image)")
+        cell.nameLabel.text = arrayDish[indexPath.row].name
+        let m = "\(arrayDish[indexPath.row].time) min"
+        let attributedString = NSMutableAttributedString(string: m)
+        let range = NSRange(location: 3, length: 3)
+        attributedString.setAttributes([NSAttributedString.Key.baselineOffset: 8], range: range)
+        attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 12), range: range)
+        attributedString.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: 17), range: NSRange(location: 0, length: 2))
+        
+        
+        cell.timeLabel.attributedText = attributedString
+        cell.levelLabel.text = arrayDish[indexPath.row].level
+        return cell
+    }
+    
+}
--- MyDish/ViewController.swift
+++ MyDish/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+class ViewController: UIViewController{
     
     var arrayDish: [Dish] = {
         let data = TakeDataToMainView()
@@ -17,22 +17,9 @@ class ViewController: UIViewController,UICollectionViewDataSource,UICollectionVi
     }()
     
     override func viewDidLoad() {
+    
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print("se")
-        print(arrayDish.count)
-        return arrayDish.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MainDishCollectionViewCell
-        cell.dishImage.image = UIImage(named: "\(arrayDish[indexPath.row].image)")
-        cell.nameLabel.text = arrayDish[indexPath.row].name
-        cell.timeLabel.text = arrayDish[indexPath.row].time
-        cell.levelLabel.text = arrayDish[indexPath.row].level
-        return cell
-    }
 }
 
 
