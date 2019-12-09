16d8a5dbd6c24080e4d94675ea4415733fc41319
Pawel <pawel3007@vp.pl>
2019-12-09 20:27:13 +0100

    add products list

---
+++ MyDish/Controller/NeedsProductsTable.swift
@@ -0,0 +1,9 @@
+//
+//  NeedsProductsTable.swift
+//  MyDish
+//
+//  Created by Pawel Krzywik on 09/12/2019.
+//  Copyright © 2019 Pawel Krzywik. All rights reserved.
+//
+
+import Foundation
---
+++ MyDish/View/Cells/ProductsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductsTableViewCell.swift
+//  MyDish
+//
+//  Created by Pawel Krzywik on 09/12/2019.
+//  Copyright © 2019 Pawel Krzywik. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
