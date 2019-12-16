006fec6578fc22323c4ab0b0198b4fb28ce428e4
Pawel <pawel3007@vp.pl>
2019-12-16 15:59:05 +0100

    addShop works in main, now i do small refactor

---
+++ MyDish/Model/ShopListDataStruct.swift
@@ -0,0 +1,9 @@
+//
+//  ShopListDataStruct.swift
+//  MyDish
+//
+//  Created by Pawel Krzywik on 15/12/2019.
+//  Copyright © 2019 Pawel Krzywik. All rights reserved.
+//
+
+import Foundation
---
+++ MyDish/Model/ShopListStructInCache.swift
@@ -0,0 +1,9 @@
+//
+//  ShopListStructInCache.swift
+//  MyDish
+//
+//  Created by Pawel Krzywik on 15/12/2019.
+//  Copyright © 2019 Pawel Krzywik. All rights reserved.
+//
+
+import Foundation
